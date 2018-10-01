7c1ed27658007a7dbeb5b2758998e8c4e93aabb0
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-01 15:56:36 -0300

    commit test 3

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -37,6 +37,7 @@ class SampleTest extends \PHPUnit_Framework_TestCase
     public function testFunction()
     {
         $target = new \App\Calculator();
+        $bbb="bbb";
         $result = $target->sum(1, 2);
         $this->assertEquals(
             3,
