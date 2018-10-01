662fdc71af0672c17769ec892522f0bb11db73e7
Paulo Daniel <paulo.daniel@paggi.com>
2018-10-01 09:20:25 -0300

    First test to be tested.

---
+++ tests/TestIndex.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file will test the Index file in src folder
+ * 
+ * PHP version 5.4
+ * 
+ * @category Description
+ * @package  Category
+ * @author   Name <email@email.com>
+ * @license  MIT www.www.www
+ * @link     http://url.com
+ */
+
+use PHPUnit\Framework\TestCase;
+use test\lib\Index;
+
+/**
+ * This class controls all test cases who will be executed.
+ * 
+ * @category Description
+ * @package  Category
+ * @author   Name <email@email.com>
+ * @license  MIT www.www.www
+ * @link     http://url.com
+ */
+final class TestIndex extends TestCase
+{
+    /**
+     * Verify if the sum is working as expected
+     *
+     * @return void
+     */
+    public function testSum()
+    {
+        $this.assertEquals(
+            5,
+            Index::testSum(2, 3)
+        );
+    }
+}
